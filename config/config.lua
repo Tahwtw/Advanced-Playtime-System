788861bb1c148b45b049bc3556700df6422744d6
Tah <89281814+Tahwtw@users.noreply.github.com>
2024-07-21 00:32:36 -0500

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -7,5 +7,5 @@ Config = {}
 Config.Settings = {
     -- The required playtime a player needs to use a weapon.
     -- Note; If your adjusting the required playtime, It will be In minutes.
-    Playtime = 69
+    Playtime = 60
 }
